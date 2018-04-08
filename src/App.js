81c66a6bbe50014cb8ac40376f06d914399ec786
Cynthia Rittenbach <crittenbach@gmail.com>
2018-04-08 23:23:31 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
 import QuoteForm from './containers/QuoteForm'
 import Quotes from './containers/Quotes'
+import { connect } from 'react-redux';
 
 export class App extends Component {
   render() {
+                              
     return (
       <div className="container-fluid">
         <div className="row title justify-content-center" style={{ paddingTop: '12px' }}>
           <h1>Quote Maker</h1>
         </div>
         <hr />
-       {
-         <div>
-           <QuoteForm />
-           <Quotes />
-         </div>
-       }
+        < QuoteForm />
+        < Quotes />                  
       </div>
     );
   }
